0d7d6bd3b82dc65617a72bdcbb28f5d60551a12b
Marcus Hott <marcus.hott@student.ecp.fr>
2018-11-10 21:55:51 +0100

    separate json files for train, validate and test

--- code/image2json.py
+++ code/image2json.py
@@ -1,28 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 10 14:08:09 2018
 
-@author: Marcus
-"""
-
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import json
 import glob
+from random import shuffle
 
 #load all images into the image_list
 image_list = []
 #load all images into the image_list
-for images in glob.glob('../datasets/img_raw/*.jpg'): 
-    #load image into pixel_x * pixel_y * 3 array
-    current_im = plt.imread(images)
-    print(current_im.shape)
-    image_list.append(current_im.tolist())
-    
-data = [{"id": image_id,
-            "img": image,
-            "label": 1} for image_id,image in zip(range(len(image_list)),image_list)] 
-    
-with open('json2.json', 'w') as outfile:  
+for images in glob.glob('data/*.jpg'): 
+    name_image = images[5:]
+    image_list.append(name_image)
+
+#randomly shuffle the list
+shuffle(image_list)
+#assign train, validate and test set
+number_images = len(image_list)
+step1 = int(0.8*number_images)
+step2 = int(0.9*number_images)
+test_list = image_list[step1:step2]
+validate_list = image_list[step2:]
+image_list = image_list[:step1]
+
+#add images to train, validate and test json object
+train_data = [{"img": image,
+            "label": 1} for image in image_list] 
+validate_data = [{"img": image,
+            "label": 1} for image in validate_list]
+test_data = [{"img": image,
+            "label": 1} for image in test_list]
+
+print(validate_data)
+
+#encode json objects and write to files
+with open('train.json', 'w') as outfile:  
     json.dump(data, outfile)
+with open('validate.json', 'w') as outfile:  
+    json.dump(data, outfile)
+with open('test.json', 'w') as outfile:  
+    json.dump(data, outfile)
+    
+    
